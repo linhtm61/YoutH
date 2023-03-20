fceae77ccafb1c05e56108367e0380e7a84ba26d
linhtm61 <128112888+linhtm61@users.noreply.github.com>
2023-03-20 22:38:55 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+let myname = document.getElementById('myname').value;
+console.log(myname);
+
+let email = document.getElementById('email').value;
+console.log(email);
+
+let note = document.getElementById('note').value;
+console.log(note);
